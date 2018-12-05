0c8857ef0ef8a34f750b3f79bac344d8a56b79a1
Ola Karlsson <ola@olakarlsson.com>
2018-12-05 16:51:49 +0100

    Added files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+//This is just a bit of Javacsript to show that it works
+let h1Text = "This is a demo";
+
+let h1Element = document.getElementById("first-h1");
+h1Element.textContent = h1Text;
+
+
+//Check if there is support for Service workers and if so, load service worker,
+//the service worker will add off line support
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/js/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
